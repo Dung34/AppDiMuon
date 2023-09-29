5f457b05dab4699ed4c7bc7ae1620318317f3f2c
liguang612 <p.lyquangk.lythedan@gmail.com>
2023-09-29 10:27:07 +0700

    Tạo Repository và các Constant

---
+++ lib/data/repository/remote/okr_repository_imp.dart
@@ -0,0 +1,127 @@
+import '../../../domain/entity/okr/key_result/key_result.dart';
+import '../../../domain/entity/okr/objective/objective.dart';
+import '../../../domain/entity/project/task.dart';
+import '../../model/api/base_response.dart';
+import 'okr_repository.dart';
+
+class OKRRepositoryImpl extends OKRRepository {
+  @override
+  Future<ResponseWrapper<KeyResult>> createKeyResult() {
+    // TODO: implement createKeyResult
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<Objective>> createObjective() {
+    // TODO: implement createObjective
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<Task>> createTask() {
+    // TODO: implement createTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<int>> deleteKeyResult() {
+    // TODO: implement deleteKeyResult
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<int>> deleteObjective() {
+    // TODO: implement deleteObjective
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<int>> deleteTask() {
+    // TODO: implement deleteTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<List<Objective>>> getAllChildObjectiveOfAnObjective() {
+    // TODO: implement getAllChildObjectiveOfAnObjective
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<List<KeyResult>>> getAllKeyResultOfObjective() {
+    // TODO: implement getAllKeyResultOfObjective
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<List<Objective>>> getAllObjectiveOfOKR() {
+    // TODO: implement getAllObjectiveOfOKR
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<List<Objective>>> getAllObjectiveOfParentUnit() {
+    // TODO: implement getAllObjectiveOfParentUnit
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<List<Task>>> getAllRelatedTaskOfTask() {
+    // TODO: implement getAllRelatedTaskOfTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<List<Task>>> getAllTaskOfKR() {
+    // TODO: implement getAllTaskOfKR
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<List<Task>>> getAllTaskOfSubtask() {
+    // TODO: implement getAllTaskOfSubtask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<List<Task>>> getAllTaskOfUser() {
+    // TODO: implement getAllTaskOfUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<KeyResult>> getKeyResultDetails() {
+    // TODO: implement getKeyResultDetails
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<Objective>> getObjectiveDetails() {
+    // TODO: implement getObjectiveDetails
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<Task>> getTaskDetails() {
+    // TODO: implement getTaskDetails
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<int>> updateKeyResult() {
+    // TODO: implement updateKeyResult
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<int>> updateObjective() {
+    // TODO: implement updateObjective
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ResponseWrapper<int>> updateTask() {
+    // TODO: implement updateTask
+    throw UnimplementedError();
+  }
+}
